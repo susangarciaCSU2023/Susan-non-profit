66831cb3f803e53f1f98791e1d12ec21febbe2d8
Susan Garcia <garcia_susan@students.columbusstate.edu>
2023-07-08 17:03:54 +0000

    Changes

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def hello():
 def home():
     return render_template("index.html")
 
+@app.route("/page")
+def page1():
+    return render_template("donate.html")
+
+@app.route("/shopping")
+def page2():
+    return render_template("shop.html")
+
 if __name__ == "__main__":
     app.run()
  
